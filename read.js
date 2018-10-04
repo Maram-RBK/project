ef83a20796d285fe529c219e46a1e349a607861f
maram_rafe <maroomah1010@gmail.com>
2018-10-04 12:08:46 +0300

    1

---
+++ read.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+$('#b2').click(History);
+})
+
+var History = function(){
+	$('span').append('<h1> kgsslkj <h2>')
+}
+
+var arr = ["A reader lives a thousand lives before he dies", "We read to know we're not alone", "If one cannot enjoy reading a book over and over again, there is no use in reading it at all"];
